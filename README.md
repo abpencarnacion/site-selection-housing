3570f3c90a579d224c1ebfd37134711f1d932217
Alyssa Bianca Encarnacion <alyssa.encarnacion@emma-sleep.com>
2023-04-29 17:17:06 +0800

    first working files of the repo. still a WIP

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# site-selection-housing
+# Site Selection for Housing (STILL A WIP) #
+### The tool is meant to help the user compare housing options based on: a) the monthly rent of the unit; b) ease/convenience of transit to the destination; c) access to common necessities (groceries, restaurants) ###
+
+### --- ###
+
+# Potential Users #
+### 1. Incoming university students choosing a studio/dorm relative to their campus ###
+### 2. Employees choosing residence relative to their new office ###
+
+### --- ###
+
+# Focus for Coding Practice #
+### 1. Classes ###
+### 2. Google Maps API ###
+### 2. Streamlit ###
+
+### --- ###
+
+# Intended Flow #
+### 1. The user inputs the address of the housing options (origins) and the address of the target university/office (destination) ###
+### 2. The user should see a table summary of the monthly rents, transit routes, and common necessities for the housing options ###
+### 3. xxxxx ###
+
+### --- ###
---
+++ src/gmaps_helpers.py
@@ -0,0 +1,57 @@
+import googlemaps
+import requests
+import html
+import json
+
+class Location:
+
+
+    def __init__(self, filepath, loc_names_list, loc_coords_list):
+        
+        ### Use interim method since we will not be making an API call
+        ## Initialize the filepath to be manually declared
+        self.filepath = filepath
+        
+        ## Initialize the lists to which we'll append the API responses later
+        self.loc_names_list = loc_names_list
+        self.loc_coords_list = loc_coords_list
+
+    
+    def get_response(self):
+
+        ### Use interim json response to save on API calls ###
+        ## Open the saved json response then access results sub-array
+        with open(self.filepath) as f:
+            data = json.load(f)
+        self.response = data['results']
+    
+
+    def get_name_from_response(self):
+
+        ## Count the number of results came back from API call
+        loops = len(self.response)
+        
+        ## For each result, get the name and append to the above list
+        for l in range(0,loops):
+            self.loc_names_list.append(self.response[l]['name'])
+    
+
+    def get_coords_from_response(self):
+
+        ## Count the number of results came back from API call
+        loops = len(self.response)
+        
+        ## For each result, get the coords/lat-lon pair and append to the above list
+        for l in range(0,loops):
+            coords_pair = []
+            coords_pair.append(self.response[l]['geometry']['location']['lng'])
+            coords_pair.append(self.response[l]['geometry']['location']['lat'])
+            self.loc_coords_list.append(coords_pair)
+    
+
+    def loc_main(self):
+
+        ## All methods in sequence
+        self.get_response()
+        self.get_name_from_response()
+        self.get_coords_from_response()
